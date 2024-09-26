7a1e5c96f801e7d2a4228ad66f60bca2e74943b9
aliciviltech <ali.civiltech@gmail.com>
2024-09-26 18:49:42 +0500

    2nd commit - website made responsive

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,53 +1,67 @@
 import React, { useState } from 'react'
 import './Home.css'
+import useWindowSize from './windowWidth/WindowWidth'
 import { allSmoothies } from '../../utils/smoothiesData'
 
 const Home = () => {
+    const { width } = useWindowSize();
+    console.log(width)
+    const windowWidth = useState(window.innerWidth);
+    console.log(windowWidth[0]);
+
+    const [isNav, setIsNav] = useState(false);
+    const displayNavF=()=>{
+        setIsNav(true);
+    }
     const [targetSmoothie, setTargetSmoothie] = useState(allSmoothies[0]);
-    const smoothieF = (id)=>{
-        const requiredSmoothie = allSmoothies.find((smoothie)=>{
+    const smoothieF = (id) => {
+        const requiredSmoothie = allSmoothies.find((smoothie) => {
             return smoothie.id === id
         })
         setTargetSmoothie(requiredSmoothie);
-        console.log(requiredSmoothie)
     }
     return (
-        <div className='Home' style={{backgroundImage:`url(${targetSmoothie.bg_image})`}}>
+        <div className='Home' style={{ backgroundImage: `url(${targetSmoothie.bg_image})` }}>
             {/* =============== header ================ */}
             <div className='Header'>
                 <div className="logo">
                     <img src="/asset/images/logo.png" alt="" />
                 </div>
 
-                <div className="anchors">
-                    <a style={{fontWeight:'900'}} href="">Home</a>
+                <div className={`anchors ${isNav && "displayNav"}`} style={{backgroundColor:`${width<600 ? targetSmoothie.color : 'transparent'}`}}>
+                    <a style={{ fontWeight: '900' }} href="">Home</a>
                     <a href="">Specialities</a>
                     <a href="">About</a>
                     <a href="">Contact us</a>
+                </div>
+                <div className="menuBars" onClick={()=>setIsNav(!isNav)} style={{color:`${ targetSmoothie.color}`}}>
+                    <i className='fa-solid fa-bars'></i>
                 </div>
             </div>
 
             {/* =============== hero section ============ */}
             <div className='HeroIntro'>
-            <div className="leftContent">
-                <div className="content">
-                    <img src='/asset/images/content.png' alt="" />
+                <div className="leftContent">
+                    <div className="content">
+                        <img src='/asset/images/content.png' alt="" />
+                        <p>Smoothies are so healthy, refreshing and the perfect way to pack some extra fruit and veggies into your diet.</p>
+                        <button style={{backgroundColor:`${targetSmoothie.color}`}}>Order Now</button>
+                    </div>
+                    <div className="itemChoices">
+                        {allSmoothies.map((smoothie) => {
+                            return (
+                                <div>
+                                    <img onClick={() => smoothieF(smoothie.id)} key={smoothie.id} src={smoothie.image} alt="" />
+                                    <div className={targetSmoothie.id === smoothie.id ? 'activeLine' : 'inactive'}></div>
+                                </div>
+                            )
+                        })}
+                    </div>
                 </div>
-                <div className="itemChoices">
-                    {allSmoothies.map((smoothie)=>{
-                        return (
-                        <div>
-                            <img onClick={()=> smoothieF(smoothie.id)} key={smoothie.id} src={smoothie.image} alt="" />
-                            <div className={targetSmoothie.id === smoothie.id ? 'activeLine' : 'inactive'}></div>
-                        </div>
-                        )
-                    })}
+                <div className="rightContent">
+                    <img src='/asset/images/bowl_group2.png' className={targetSmoothie.rotate} alt="" />
                 </div>
             </div>
-            <div className="rightContent">
-                <img src='/asset/images/bowl_group2.png'  /*targetSmoothie.image*/ style={{rotate:`${targetSmoothie.rotate}`}} alt="" />
-            </div>
-        </div>
         </div>
     )
 }
--- src/utils/smoothiesData.js
+++ src/utils/smoothiesData.js
@@ -2,24 +2,28 @@ const smoothie1 = {
     id:'1',
     image: '/asset/images/bowl1.png',
     bg_image: './asset/images/bg_bowl1.png',
-    rotate: '-10deg'
+    rotate: 'rotation1',
+    color:'#f66c9e',
 }
 const smoothie2 = {
     id:'2',
     image: '/asset/images/bowl2.png',
     bg_image: './asset/images/bg_bowl2.png',
-    rotate: '-110deg'
+    rotate: 'rotation2',
+    color:'#feac00',
 }
 const smoothie3 = {
     id:'3',
     image: '/asset/images/bowl3.png',
     bg_image: './asset/images/bg_bowl3.png',
-    rotate: '-190deg'
+    rotate: 'rotation3',
+    color:'#799dfa',
 }
 const smoothie4 = {
     id:'4',
     image: '/asset/images/bowl4.png',
     bg_image: './asset/images/bg_bowl4.png',
-    rotate: '-280deg'
+    rotate: 'rotation4',
+    color:'#1ae06d',
 }
 export const allSmoothies = [smoothie1, smoothie2, smoothie3, smoothie4];
